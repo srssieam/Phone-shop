b42aebac194bad2c2bba9aabdcde24ce348ae1e0
srs-sieam <srssieam@gmail.com>
2023-09-23 17:03:21 +0600

    created main layout for this wabsite and added home component with same path

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Home/Home";
 
 const myCreatedRouter = createBrowserRouter([
     {
         path:'/',
-        element: <div>hello world</div>
+        element: <MainLayout></MainLayout>,  // Inside this component other component will remain fixed and displayed always except <outlet/>
+        children:[
+            {
+                path:'/',   // here we have to use same path to display the beginning page of the website
+                element: <Home></Home>   // This component will display at the beginning  
+            }
+        ]
     }
 ])
 export default myCreatedRouter;
