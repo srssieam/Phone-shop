92d5eb99e19ddb2587b11bea0902d3c775f422b4
srs-sieam <srssieam@gmail.com>
2023-09-24 19:07:59 +0600

    added favouritecart

--- src/pages/Favourite/Favourite.jsx
+++ src/pages/Favourite/Favourite.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
+import FavouriteCart from "./FavouriteCart";
 
 
 const Favourite = () => {
+    const [favorite, setFevourite]= useState([]);
+    const [noFound, setNoFound]= useState();
+
+    useEffect(()=>{
+        const favouriteItem =JSON.parse(localStorage.getItem('favourite'));
+        
+        if(favouriteItem){
+            setFevourite(favouriteItem)
+        }else{
+            setNoFound('No data found')
+        }
+
+    },[])
     return (
         <div>
             
+            {/* {noFound ? (<p className="h-[80vh] flex justify-center items-center">{noFound}</p>) : (<div></div>)} */}
+            {noFound ? (<p className="h-[80vh] flex justify-center items-center">{noFound}</p>) : 
+            (<div className="grid grid-cols-2 gap-5">
+                {favorite.map( phone => <FavouriteCart key={phone.id} favouritePhone={phone}></FavouriteCart>)}
+            </div>)}
         </div>
     );
 };
