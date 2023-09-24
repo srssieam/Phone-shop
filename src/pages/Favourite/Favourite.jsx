98900b74af575eef657cc938623e9665ad66a2ad
srs-sieam <srssieam@gmail.com>
2023-09-24 19:17:56 +0600

    Removing process added

--- src/pages/Favourite/Favourite.jsx
+++ src/pages/Favourite/Favourite.jsx
@@ -16,16 +16,26 @@ const Favourite = () => {
         }
 
     },[])
+
+    const handleRemove =()=>{
+        localStorage.clear()
+        setFevourite([])
+    }
     return (
         <div>
             
+            {
+                favorite.length > 0 && 
+                <div><button onClick={handleRemove} className="px-5 bg-green-200 block mx-auto">Deleted All favorites</button></div>
+                }
             {/* {noFound ? (<p className="h-[80vh] flex justify-center items-center">{noFound}</p>) : (<div></div>)} */}
             {noFound ? (<p className="h-[80vh] flex justify-center items-center">{noFound}</p>) : 
             (<div className="grid grid-cols-2 gap-5">
+
                 {favorite.map( phone => <FavouriteCart key={phone.id} favouritePhone={phone}></FavouriteCart>)}
             </div>)}
         </div>
-    );
+    )
 };
 
 export default Favourite;
