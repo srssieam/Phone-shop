eb528f05dc26e1191d16f8930176750f1f390540
srs-sieam <srssieam@gmail.com>
2023-09-24 07:30:28 +0600

    passed phones data to Phones.jsx component and received phones data

--- src/Component/Phones/Phones.jsx
+++ src/Component/Phones/Phones.jsx
@@ -1,6 +1,7 @@
 
 
-const Phones = () => {
+const Phones = ({phones}) => {
+    console.log(phones)
     return (
         <div>
             <h1 className="text-2xl text-center py-10">Our Phones Collections</h1>
