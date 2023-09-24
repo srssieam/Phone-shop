f2d08d41e42ab26536be469f54f7a2ffb6868981
srs-sieam <srssieam@gmail.com>
2023-09-24 11:57:18 +0600

    received data from Phones.jsx component and copy a card form material-tailwindcss.com

--- src/Component/Phones/Phones.jsx
+++ src/Component/Phones/Phones.jsx
@@ -6,7 +6,7 @@ const Phones = ({phones}) => {
     return (
         <div>
             <h1 className="text-2xl text-center py-10">Our Phones Collections</h1>
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10 gap-2">
                 {   
                     // note: always use optional chaining to escape from 'map is not a function error'
                     phones?.map(phone =><PhoneCard key={phone.id} phone={phone}></PhoneCard>)  
