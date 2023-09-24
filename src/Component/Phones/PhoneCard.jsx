3b5b3a056f979655defd2e6489d932d20290d314
srs-sieam <srssieam@gmail.com>
2023-09-24 12:03:20 +0600

    created dynamic link for buttons

--- src/Component/Phones/PhoneCard.jsx
+++ src/Component/Phones/PhoneCard.jsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 
 const PhoneCard = ({ phone }) => {
-    const { image, phone_name, brand_name, price, rating } = phone || {};  // used '|| {}' to escape error
+    const { id, image, phone_name, brand_name, price, rating } = phone || {};  // used '|| {}' to escape error
     return (
         <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
             <div className="relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
@@ -26,12 +27,14 @@ const PhoneCard = ({ phone }) => {
                 </p>
             </div>
             <div className="p-6 pt-0">
+                <Link to={`/phones/:${id}`}>  {/* here we used id to create dynamic links */}
                 <button
                     className="block w-full select-none rounded-lg bg-gray-300 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     type="button"
                 >
                     See details
                 </button>
+                </Link>
             </div>
         </div>
     );
