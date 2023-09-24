94474b9b8d6f44b4bb06ebca5579f16187d2cb6b
srs-sieam <srssieam@gmail.com>
2023-09-24 07:41:34 +0600

    map phones data and passed to PhoneCard component

---
+++ src/Component/Phones/PhoneCard.jsx
@@ -0,0 +1,10 @@
+
+const PhoneCard = () => {
+    return (
+        <div>
+            <h3>phone</h3>
+        </div>
+    );
+};
+
+export default PhoneCard;
--- src/Component/Phones/Phones.jsx
+++ src/Component/Phones/Phones.jsx
@@ -1,3 +1,4 @@
+import PhoneCard from "./PhoneCard";
 
 
 const Phones = ({phones}) => {
@@ -5,6 +6,12 @@ const Phones = ({phones}) => {
     return (
         <div>
             <h1 className="text-2xl text-center py-10">Our Phones Collections</h1>
+            <div>
+                {   
+                    // note: always use optional chaining to escape from 'map is not a function error'
+                    phones?.map(phone =><PhoneCard key={phone.id} phone={phone}></PhoneCard>)  
+                }
+            </div>
         </div>
     );
 };
