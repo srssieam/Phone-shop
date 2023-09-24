3580e5d8f5a38cc6dbcd6cfc58c1a1f2a18dc0f1
srs-sieam <srssieam@gmail.com>
2023-09-24 19:37:27 +0600

    more toggle see more and see less

--- src/pages/Favourite/Favourite.jsx
+++ src/pages/Favourite/Favourite.jsx
@@ -3,43 +3,49 @@ import FavouriteCart from "./FavouriteCart";
 
 
 const Favourite = () => {
-    const [favorite, setFevourite]= useState([]);
-    const [noFound, setNoFound]= useState();
-    const [isShow,setIsShow] = useState(false)
+    const [favorite, setFevourite] = useState([]);
+    const [noFound, setNoFound] = useState();
+    const [isShow, setIsShow] = useState(false)
 
-    useEffect(()=>{
-        const favouriteItem =JSON.parse(localStorage.getItem('favourite'));
-        
-        if(favouriteItem){
+    useEffect(() => {
+        const favouriteItem = JSON.parse(localStorage.getItem('favourite'));
+
+        if (favouriteItem) {
             setFevourite(favouriteItem)
-        }else{
+        } else {
             setNoFound('No data found')
         }
 
-    },[])
+    }, [])
 
-    const handleRemove =()=>{
+    const handleRemove = () => {
         localStorage.clear()
         setFevourite([]);
         setNoFound('No data found')
     }
     return (
         <div>
-            
+
             {
-                favorite.length > 0 && 
+                favorite.length > 0 &&
                 <div><button onClick={handleRemove} className="px-5 bg-green-200 block mx-auto">Deleted All favorites</button></div>
-                }
+            }
             {/* {noFound ? (<p className="h-[80vh] flex justify-center items-center">{noFound}</p>) : (<div></div>)} */}
-            {noFound ? (<p className="h-[80vh] flex justify-center items-center">{noFound}</p>) : 
-            (<div className="grid grid-cols-2 gap-5">
+            {noFound ? (<p className="h-[80vh] flex justify-center items-center">{noFound}</p>) :
+                (<div className="grid grid-cols-2 gap-5">
 
-                {favorite.map( phone => <FavouriteCart key={phone.id} favouritePhone={phone}></FavouriteCart>)}
-            </div>)}
+                    {
+                        isShow ? favorite.map((phone) => (
+                            <FavouriteCart key={phone.id} favouritePhone={phone}></FavouriteCart>
+                        ))  : favorite.slice(0, 2).map((phone) => (
+                            <FavouriteCart key={phone.id} favouritePhone={phone}></FavouriteCart>
+                        ))
+                    }
+                </div>)}
 
-            {favorite.length > 2 && <button onClick={()=>setIsShow(!isShow)} className="px-5 bg-green-200 block mx-auto">
-            {isShow ? "See less" : "See more"}
-          </button>}
+            {favorite.length > 2 && <button onClick={() => setIsShow(!isShow)} className="px-5 bg-green-200 block mx-auto">
+                {isShow ? "See less" : "See more"}
+            </button>}
         </div>
     )
 };
