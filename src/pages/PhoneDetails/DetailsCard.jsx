e6eaa5ba8fd0ee4345a74fe0a7871cd5b7e590fe
srs-sieam <srssieam@gmail.com>
2023-09-24 15:54:15 +0600

    fixed problem

---
+++ src/pages/PhoneDetails/DetailsCard.jsx
@@ -0,0 +1,12 @@
+
+
+const DetailsCard = ({details}) => {
+    const {id, phone_name, brand_name, rating, price, image}= details
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DetailsCard;
--- src/pages/PhoneDetails/PhoneDetails.jsx
+++ src/pages/PhoneDetails/PhoneDetails.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import DetailsCard from "./DetailsCard";
 
 
 const PhoneDetails = () => {
-    const  { getId }  = useParams();  // we used useParams to extract the id parameter from the URL, and then we can use it in our component.
-    const phonesData = useLoaderData();
+    const  { getId }  = useParams();  // we used useParams to extract the getId parameter from the URL, and then we can use it in our component.
+    const phonesData = useLoaderData({});
     console.log(phonesData);
 
     const [phoneData, setPhoneData] = useState({});
     useEffect(() => {
-        const findPhone = phonesData?.find((phone) => phone.id === getId); // finding phone data of getId
+        const findPhone = phonesData?.find((phone) => phone.id === getId); // finding phone data of getgetId
         setPhoneData(findPhone);
-        // console.log(phoneData)
-    }, [getId, phonesData]) // here we set dependency as  getId and phonesData so that useEffect can run the function after getting values of getId and phonesData
+        console.log(findPhone)
+    }, [getId, phonesData]) // here we set dependency as  getgetId and phonesData so that useEffect can run the function after getting values of getgetId and phonesData
     
     return (
         <div>
-            <h1>Phone details</h1>
+            <DetailsCard details={phoneData} ></DetailsCard>
         </div>
     );
 };
