cfbecd9c1deaac30cc712c6ee1edf4873d351302
srs-sieam <srssieam@gmail.com>
2023-09-24 21:11:52 +0600

    created dynamic title using useLocation hook

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,7 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Component/Header/Header";
+import { useEffect } from "react";
 
 const MainLayout = () => {
+    const loc = useLocation()
+    useEffect(()=>{
+        document.title = loc.pathname;
+    },[loc.pathname])
+
     return (
         <div>
             <Header></Header>
